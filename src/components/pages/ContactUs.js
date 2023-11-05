657d91bc2b0a89a6739e3e9651ab29ef45d1d3a6
Shriya Wagholikar <swagholikar084@student.fuhsd.org>
2023-11-05 14:44:34 -0600

    im sending over the folder that will have all my contact form code

--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../../App.css';
+import { keyboard } from '@testing-library/user-event/dist/keyboard';
 
 export default function ContactUs() {
   return <h1 className='contact-us'>CONTACT</h1>;
+
+  
 }
